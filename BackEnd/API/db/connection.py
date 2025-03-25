df594de34c6305dc3cfccc623c885ba530ead4bf
Sayemahamed <sayemahamed183@gmail.com>
2025-03-25 20:51:14 +0600

    added entity diagram

---
+++ BackEnd/API/db/connection.py
@@ -0,0 +1,25 @@
+from typing import Any, AsyncGenerator
+
+from API.config import settings
+from click import echo
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+async_engine: AsyncEngine = create_async_engine(
+    url=settings.POSTGRES_URL_ASYNC, echo=True
+)
+
+
+async def init_db() -> None:
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, Any]:
+    async_session = async_sessionmaker(
+        bind=async_engine, class_=AsyncSession, expire_on_commit=False
+    )
+    async with async_session() as session:
+        yield session
--- BackEnd/API/main.py
+++ BackEnd/API/main.py
@@ -1,4 +1,19 @@
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from rich import print
+from fastapi import FastAPI
+from API.db import init_db
 
 @asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("[green]Starting up...")
+    await init_db()
+    yield
+    print("[green]Shutting down...")
+
+version ="v1"
+app = FastAPI(
+    title="Price Pilot",
+    lifespan=lifespan
+    ,version=version,
+    description="An AI-Powered E-Commerce Procurement Assistant",
+    )
